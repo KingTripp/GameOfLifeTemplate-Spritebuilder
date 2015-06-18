3b60bbf1cdd7c2b019481ef4743da46a09637d0f
KingTripp <terrell.guinn@lander.edu>
2015-06-18 18:50:37 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/GameOfLife.approj/Grid.h
@@ -0,0 +1,13 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Tripp Guinn on 6/18/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@end
---
+++ GameOfLife.spritebuilder/GameOfLife.approj/Grid.m
@@ -0,0 +1,13 @@
+//
+//  Grid.m
+//  GameOfLife
+//
+//  Created by Tripp Guinn on 6/18/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Grid.h"
+
+@implementation Grid
+
+@end
